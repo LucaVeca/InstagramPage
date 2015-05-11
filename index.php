74e515ec804f628882680dc3a1ef5543c7e22869
Luca Veca <lucaveca@gmail.com>
2015-05-11 11:54:13 -0700

    Milestone 2

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+	//Configuration for PHP server
+	set_time_limit(0);
+	ini_set('default_socket_timeout', 300);
+	//starts session
+	session_start();
+	//Make Constants using define
+	define('client_ID', '025e77c43f93484fb23e475d3530c15c');
+	define('client_Secret', '952be6ecb7d941f89de536e5fdd9d75e');
+	define('redirectURI', 'http://localhost/maxapi/index.php');
+	define('ImageDirectory', 'pics/');
+?>
+
+CLIENT ID	ebd393ddcaba4500a056cfa0ce5b944d
+CLIENT SECRET	50f0ba8f32b0494588f35a13cdf7bd7b
+WEBSITE URL	http://localhost/instagrampage/index.php
+REDIRECT URI	http://localhost/instagrampage/index.php
